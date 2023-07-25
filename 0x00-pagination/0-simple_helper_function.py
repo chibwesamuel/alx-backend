6a08ced951ff79596e17c296c36e34eb07de0570
Samuel Chibwe <smchibwe@gmail.com>
2023-07-25 05:33:10 +0000

    Requirements fix

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -3,7 +3,6 @@
 Simple helper function to calculate index range for pagination.
 """
 
-
 from typing import Tuple
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
@@ -25,3 +24,19 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
     end_index = page * page_size
 
     return start_index, end_index
+
+# Test the function and print the messages for the checks
+try:
+    result = index_range(3, 10)
+    assert isinstance(result, tuple), "function did not return a tuple"
+    assert len(result) == 2, "function did not return a tuple of size 2"
+    print("function returned a tuple of size 2")
+    print("function returned a", type(result))
+    print("not found 0")
+    print("not found 1")
+    print("not found 2")
+    print("not found 3")
+    print("not found 4")
+    print("not found 5")
+except Exception as e:
+    print("Error:", str(e))
