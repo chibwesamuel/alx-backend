8eb47ff4b03c4bb1c3bf90072ae01e82f3aad32f
Samuel Chibwe <smchibwe@gmail.com>
2023-07-25 16:24:54 +0000

    Pagination Error Fix

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -4,12 +4,13 @@ Implement a method named get_page that takes two integer arguments page
 with default value 1 and page_size with default value 10.
 """
 
+
 import csv
 from typing import List
 
 class Server:
     """Server class to paginate a database of popular baby names."""
-    
+
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
@@ -26,26 +27,42 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """Return the appropriate page of the dataset based on pagination
-        parameters.
-
-        Args:
-            page (int): The page number (1-indexed).
-            page_size (int): The number of items per page.
-
-        Returns:
-            List[List]: A list containing the rows for the given
-            page and page_size.
-        """
+        """Get a page of the dataset based on page number and page size."""
         assert isinstance(page, int) and page > 0, "Page must be a
-        positive integer."
-        assert isinstance(page_size, int) and page_size > 0, "Page_size must
-        be a positive integer."
+        positive integer"
+        assert isinstance(page_size, int) and page_size > 0, "Page size
+        must be a positive integer"
 
-        start_index, end_index = index_range(page, page_size)
-        dataset = self.dataset()
-        if start_index >= len(dataset):
-            return []
+        index_range = index_range(page, page_size)
+        return self.__dataset[index_range[0]:index_range[1]]
 
-        return (dataset[start_index:end_index])
+def index_range(page: int, page_size: int) -> tuple:
+    """Return a tuple of start and end indexes for the given
+    page and page size."""
+    start_index = (page - 1) * page_size
+    end_index = page * page_size
+    return start_index, end_index
+
+if __name__ == "__main__":
+    server = Server()
+
+    try:
+        should_err = server.get_page(-10, 2)
+    except AssertionError:
+        print("AssertionError raised with negative values")
+
+    try:
+        should_err = server.get_page(0, 0)
+    except AssertionError:
+        print("AssertionError raised with 0")
+
+    try:
+        should_err = server.get_page(2, 'Bob')
+    except AssertionError:
+        print("AssertionError raised when page and/or
+                page_size are not ints")
+
+    print(server.get_page(1, 3))
+    print(server.get_page(3, 2))
+    print(server.get_page(3000, 100))
 
