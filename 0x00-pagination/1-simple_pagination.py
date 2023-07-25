0e2c74f3a43425c234c3d1e2d0d3f80f19a5d4b5
Samuel Chibwe <smchibwe@gmail.com>
2023-07-25 17:07:13 +0000

    Pagination Error Fix

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+
 """
 Implement a method named get_page that takes two integer arguments page
 with default value 1 and page_size with default value 10.
@@ -8,16 +9,28 @@ import csv
 import math
 from typing import List, Tuple
 
+
 class Server:
-    """Server class to paginate a database of popular baby names."""
+    """Server class to paginate a database of popular baby names.
+
+    Attributes:
+        DATA_FILE (str): The CSV file containing the dataset.
+        __dataset (List[List[str]]): Cached dataset loaded from the CSV file.
+    """
 
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
+        """Initialize the Server class."""
         self.__dataset = None
 
     def dataset(self) -> List[List[str]]:
-        """Cached dataset"""
+        """Cached dataset.
+
+        Returns:
+            List[List[str]]: The dataset loaded from the CSV file,
+            excluding the header row.
+        """
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
@@ -27,7 +40,16 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List[str]]:
-        """Get a page of the dataset based on page number and page size."""
+        """Get a page of the dataset based on page number and page size.
+
+        Args:
+            page (int, optional): Page number (default is 1).
+            page_size (int, optional): Number of items per page
+            (default is 10).
+
+        Returns:
+            List[List[str]]: A list of rows representing the page data.
+        """
         assert isinstance(page, int), "Page must be an integer"
         assert page > 0, "Page must be a positive integer"
         assert isinstance(page_size, int), "Page size must be an integer"
@@ -42,8 +64,16 @@ class Server:
 
     def index_range(self, page: int, page_size: int) -> Tuple[int, int]:
         """
-        Return a tuple of start and end indexes for the
-        given page and page size
+        Return a tuple of start and end indexes for the given page
+        and page size.
+
+        Args:
+            page (int): Page number.
+            page_size (int): Number of items per page.
+
+        Returns:
+            Tuple[int, int]: A tuple (start_index, end_index) representing
+            the index range for the page.
         """
         start_index = (page - 1) * page_size
         end_index = page * page_size
