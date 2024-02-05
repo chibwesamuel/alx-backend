505681f3cee054441c54d541cf0816cbfa2a7ec8
chibwesamuel <smchibwe@yahoo.com>
2024-02-05 18:51:11 +0200

    Node Redis client and advanced operations submission

---
+++ 0x03-queuing_system_in_js/4-redis_advanced_op.js
@@ -0,0 +1,61 @@
+import redis from 'redis';
+
+// Create a Redis client
+const client = redis.createClient();
+
+// Event listener for successful connection
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+// Event listener for connection errors
+client.on('error', (err) => {
+  console.error(`Redis client not connected to the server: ${err}`);
+});
+
+// Create Hash
+client.hset(
+  'HolbertonSchools',
+  'Portland',
+  50,
+  redis.print
+);
+client.hset(
+  'HolbertonSchools',
+  'Seattle',
+  80,
+  redis.print
+);
+client.hset(
+  'HolbertonSchools',
+  'New York',
+  20,
+  redis.print
+);
+client.hset(
+  'HolbertonSchools',
+  'Bogota',
+  20,
+  redis.print
+);
+client.hset(
+  'HolbertonSchools',
+  'Cali',
+  40,
+  redis.print
+);
+client.hset(
+  'HolbertonSchools',
+  'Paris',
+  2,
+  redis.print
+);
+
+// Display Hash
+client.hgetall('HolbertonSchools', (err, reply) => {
+  if (err) {
+    console.error(`Error getting hash from Redis: ${err}`);
+  } else {
+    console.log(reply);
+  }
+});
